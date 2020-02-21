655a8067aa54556513efc2940f7f641d0b86d996
arleyribeiro <arley.sribeiro@gmail.com>
2020-02-21 12:51:14 -0300

    2 - Making the dropdown accessible

--- src/app/custom-dropdown/custom-select-option/custom-select-option.component.ts
+++ src/app/custom-dropdown/custom-select-option/custom-select-option.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Input, HostBinding, HostListener } from '@angular/core';
 import { CustomSelectComponent } from '../custom-select/custom-select.component';
 import { CustomDropdownService } from '../custom-dropdown.service';
+import { Highlightable } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'custom-select-option',
   template: '{{value}}',
   styleUrls: ['./custom-select-option.component.scss']
 })
-export class CustomSelectOptionComponent implements OnInit {
+export class CustomSelectOptionComponent implements OnInit, Highlightable {
 
   constructor(private dropdownService: CustomDropdownService) {
     this.select = this.dropdownService.getSelect();
@@ -29,10 +30,25 @@ export class CustomSelectOptionComponent implements OnInit {
   private select: CustomSelectComponent;
     
   @HostListener('click', ['$event'])
-  public onClick(event: UIEvent) {
-  event.preventDefault();
-  event.stopPropagation();
-  
-  this.select.selectOption(this);
+    public onClick(event: UIEvent) {
+    event.preventDefault();
+    event.stopPropagation(); 
+    
+    this.select.selectOption(this);
+  }
+
+  @HostBinding('class.active')
+  public active = false;
+ 
+  public getLabel(): string {
+    return this.value;
+  }
+ 
+  public setActiveStyles(): void {
+    this.active = true;
+  }
+ 
+  public setInactiveStyles(): void {
+    this.active = false;
   }
 }
--- src/app/custom-dropdown/custom-select/custom-select.component.ts
+++ src/app/custom-dropdown/custom-select/custom-select.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, ViewChild, ElementRef, ContentChildren, Query
 import { CustomDropdownComponent } from '../custom-dropdown.component';
 import { CustomSelectOptionComponent } from '../custom-select-option/custom-select-option.component';
 import { CustomDropdownService } from '../custom-dropdown.service';
+import { ActiveDescendantKeyManager } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'custom-select',
@@ -35,6 +36,8 @@ export class CustomSelectComponent implements OnInit, AfterViewInit {
   @ContentChildren(CustomSelectOptionComponent)
   public options: QueryList<CustomSelectOptionComponent>
   
+  private keyManager: ActiveDescendantKeyManager<CustomSelectOptionComponent>
+
   constructor(private dropdownService: CustomDropdownService) {
     this.dropdownService.register(this);
   }
@@ -50,12 +53,22 @@ export class CustomSelectComponent implements OnInit, AfterViewInit {
    setTimeout(() => {
      this.selectedOption = this.options.toArray().find(option => option.key === this.selected);
      this.displayText = this.selectedOption ? this.selectedOption.value : '';
-   });
+     this.keyManager = new ActiveDescendantKeyManager(this.options)
+     .withHorizontalOrientation('ltr')
+     .withVerticalOrientation()
+     .withWrap();
+    });
   }
 
   public showDropdown() {
     this.dropdown.show();
-  }
+    
+    if (!this.options.length) {
+      return;
+    }
+    
+    this.selected ? this.keyManager.setActiveItem(this.selectedOption) : this.keyManager.setFirstItemActive();
+   }
   
   public onDropMenuIconClick(event: UIEvent) {
     event.stopPropagation();
@@ -66,6 +79,7 @@ export class CustomSelectComponent implements OnInit, AfterViewInit {
   }
 
   public selectOption(option: CustomSelectOptionComponent) {
+    this.keyManager.setActiveItem(option);
     this.selected = option.key;
     this.selectedOption = option;
     this.displayText = this.selectedOption ? this.selectedOption.value : '';
@@ -76,4 +90,31 @@ export class CustomSelectComponent implements OnInit, AfterViewInit {
   public hideDropdown() {
     this.dropdown.hide();
   }
+
+  public onKeyDown(event: KeyboardEvent) {
+    if (['Enter', ' ', 'ArrowDown', 'Down', 'ArrowUp', 'Up'].indexOf(event.key) > -1) {
+      if (!this.dropdown.showing) {
+        this.showDropdown();
+        return;
+      }
+    
+      if (!this.options.length) {
+        event.preventDefault();
+        return;
+      }
+    }
+    
+    if (event.key === 'Enter' || event.key === ' ') {
+      this.selectedOption = this.keyManager.activeItem;
+      this.displayText = this.selectedOption ? this.selectedOption.value : '';
+      this.hideDropdown();
+    } else if (event.key === 'Escape' || event.key === 'Esc') {
+      this.dropdown.showing && this.hideDropdown();
+    } else if (['ArrowUp', 'Up', 'ArrowDown', 'Down', 'ArrowRight', 'Right', 'ArrowLeft', 'Left']
+      .indexOf(event.key) > -1) {
+      this.keyManager.onKeydown(event);
+    } else if (event.key === 'PageUp' || event.key === 'PageDown' || event.key === 'Tab') {
+      this.dropdown.showing && event.preventDefault();
+    }
+   }
 }
