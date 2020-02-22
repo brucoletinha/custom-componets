4804ed433f17aab474b72048fa6c6e1427e5c2aa
arleyribeiro <arley.sribeiro@gmail.com>
2020-02-21 21:45:35 -0300

    Exemplos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +10,28 @@ export class AppComponent {
   public selectedDestination: string;
   public selectedDestination2: string;
   selectedDestination3 = new FormControl('');
+  public selectedDestination4: string;
+
+  constructor(private fb: FormBuilder) { }
+
+  profileForm = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl(''),
+  });
+
+  profileForm2 = this.fb.group({
+    item: this.fb.group({
+      id: null,
+      name: null
+    }),
+  });
+  
   title = 'angular-dropdown';
 
   data = ["Paris",  "Mauritius",  "Singapore",  "Malaysia",  "Goa",  "Thailand"];
   
   data2 = [{id: 1, name:"Paris"}, {id: 2, name:"Mauritius"}, {id: 3, name:"Singapore"},  {id: 4, name:"Malaysia"},  {id: 5, name:"Goa"},{id: 6, name:"Thailand"} ];
+  showContent() {
+    console.log(this.profileForm2.get("item").value);
+  }
 }
